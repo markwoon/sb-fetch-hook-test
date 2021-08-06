6ed5d28d7ab4d90a98fae09b1f80db5e8baaa655
Mark Woon <markwoon@gmail.com>
2021-08-06 14:45:56 -0700

    add fallbackToNetwork config

--- src/Hello.stories.js
+++ src/Hello.stories.js
@@ -14,6 +14,10 @@ export const Default = (args) => {
       name: 'Jack Reacher',
     }
   ]);
+  fetchMock.config.overwriteRoutes = true;
+
+  // UNCOMMENT THIS TO "fix" HMR:
+  //fetchMock.config.fallbackToNetwork = true;
 
   return (<Hello {...args} />);
 };
